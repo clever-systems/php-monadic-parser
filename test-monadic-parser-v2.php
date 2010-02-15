503d8ebbebf69f6db65f298ab006fd7bc1948647
Axel Rutz <xl@master.(none)>
2010-02-15 15:45:20 +0100

    v2

---
+++ test-monadic-parser-v2.php
@@ -0,0 +1,31 @@
+<?php
+include('monadic-parser-v2.php');
+
+$s='1+2*2+3';
+
+
+$number=new Token('number', '[0-9]+');
+$plus=new Token('+', '\\+');
+$times=new Token('*', '\\*');
+$openbrace=new Token('(', '\\(');
+$closebrace=new Token(')', '\\)');
+
+$simplex = new Any('simple', array($number));
+
+$product=new Chain('product', array($simplex, $times));
+$productx = new Any('productx', array($product, $simplex));
+$product->append(array($productx));
+
+$sum = new Chain('sum', array($productx, $plus));
+$sumx = new Any('sumx', array($sum, $productx));
+$sum->append(array($sumx));
+
+//$braced = new Chain('braced', array($openbrace, $productx, $closebrace));
+//$simplex->append(array($braced));
+
+$expression = $sumx;
+
+print_r($expression->parseall(new SimpleInputMapper($s), new SimpleASTMapper1()));
+
+$parserbuilder = new KAPF();
+$parserbuilder->
